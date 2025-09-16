608dd90bb1e4f96097fbfa8178d1d3945bb40c78
Aidan Reyes <aidanreyes2005@gmail.com>
2025-09-16 13:16:57 -0700

    R Code Chunks Only

--- schedule_project_template.R
+++ schedule_project_template.R
@@ -1,1 +1,8 @@
+# Setup and Data
+library(plotly)
+library(tidyverse)
 
+schedule <- read_csv("schedule.csv")
+draft_schedule <- read_csv("schedule_24_partial.csv")
+locations <- read_csv("locations.csv")
+game_data <- read_csv("team_game_data.csv")
